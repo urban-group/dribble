1fe2eda4ff979728165658572bb0755d9eabb226
Alexander Urban <alexu@freenet.de>
2013-01-16 19:19:42 -0500

    trandom transformation implemented

--- src/percol.py
+++ src/percol.py
@@ -1,27 +1,47 @@
 #!/usr/bin/env python
 
 import argparse
-import numpy as np
-import os
 import sys
 
 try:
-    from pymatgen.io.vaspio import Poscar
+    import pymatgen
 except ImportError:
     print("Unable to load the `pymatgen' module.")
     sys.exit()
 
-import pypercol
+from pymatgen.io.vaspio       import Poscar
+from pymatgen.transformations.standard_transformations \
+                              import SubstitutionTransformation
 
+from pypercol.transformations import RandomOrderingTransformation
+from pypercol                 import Percolator
 
 
 #----------------------------------------------------------------------#
 
-def percol(poscarfile):
+def percol(poscarfile, el1, el2, p1, navg):
 
-    poscar = Poscar.from_file(poscarfile)
+    subst = {el1 : {el1 : p1, el2 : (1.0-p1)}}
+
+    input_struc      = Poscar.from_file(poscarfile).structure
+    trans            = SubstitutionTransformation(subst)
+    disordered_struc = trans.apply_transformation(input_struc)
+
+    trans2           = RandomOrderingTransformation()
+
+    p_site = 0
+    p_bond = 0
+
+    for i in range(navg):
+        struc = trans2.apply_transformation(disordered_struc)
+        percolator = Percolator(struc)
+        p_site += percolator.get_site_percolation()
+        p_bond += percolator.get_bond_percolation()
     
-    
+    p_site /= navg
+    p_bond /= navg
+
+    return (p_site, p_bond)
 
 #----------------------------------------------------------------------#
 
@@ -31,10 +51,44 @@ if __name__=="__main__":
 
     parser.add_argument(
         "structure",
-        help    = "Structure in VASP's extended POSCAR format.", 
+        help    = "structure in VASP's extended POSCAR format", 
         default = "POSCAR",
         nargs   = "?")
 
+    parser.add_argument(
+        "--element1",
+        help    = "the percolating element",
+        dest    = "element1",
+        default = "Li" )
+
+    parser.add_argument(
+        "--element2",
+        help    = "the non-percolating element",
+        dest    = "element2",
+        default = "Co" )
+
+    parser.add_argument(
+        "-p", "--probability",
+        help    = "probability of percolating site/species",
+        dest    = "probability",
+        type    = float,
+        default = 0.5 )
+
+    parser.add_argument(
+        "-n", "--n-average",
+        help    = "number of random structures to average",
+        dest    = "naverage",
+        type    = int,
+        default = 10 )
+
     args = parser.parse_args()
 
-    percol(poscarfile = args.structure)
+    (p_site, p_bond) = percol( 
+        poscarfile = args.structure, 
+        el1        = args.element1,
+        el2        = args.element2,
+        p1         = args.probability,
+        navg       = args.naverage )
+
+    print p_site, p_bond
+
--- src/pypercol/__init__.py
+++ src/pypercol/__init__.py
@@ -1,19 +1,21 @@
 """
-Direct calculaion of the site and bond percolation of a crystal lattice.
+Numerical calculaion of the site and bond percolation of an 
+arbitrary crystal lattice.
 """
 
 __author__ = "Alexander Urban"
-__email__  = "alexurba@mit.edu"
 __date__   = "2013-01-15"
 
-import numpy as np
-
-class Lattice:
+class Percolator:
     
-    def __init__(avec=np.identity(3), sites=[], species=[]):
+    def __init__(self, structure):
         """
-        avec    lattice vector matrix (NumPy array)
-        sites   lattice coordinates of all sites (NumPy array)
-        species list of lists of species for each site
-                (every site can be occupied by)
+        structure     an instance of pymatgen.core.structure.Structure
         """
+        self._structure = structure
+
+    def get_site_percolation(self):
+        return 0.0
+
+    def get_bond_percolation(self):
+        return 0.0
