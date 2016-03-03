31b0e3750ef9e293bc78c92b194eb86cededdb4f
Alexander Urban <aurban@atomistic.net>
2016-03-03 13:17:16 -0800

    Old tools added to repo for the time being.

---
+++ old/anasite.py
@@ -0,0 +1,352 @@
+#!/usr/bin/env python
+
+"""
+Insert description here.
+"""
+
+from __future__ import print_function
+
+__author__ = "Alexander Urban"
+__date__   = "2013-04-05"
+
+import argparse
+import numpy as np
+import sys
+
+from scipy.spatial import Delaunay
+
+from pymatgen.io.vaspio import Poscar
+from pypercol.pynblist  import NeighborList
+
+EPS   = 100.0*np.finfo(np.float).eps
+
+#----------------- volume of an irregular tetrahedron -----------------#
+
+def tetrahedron_volume(nodes):
+    """
+    Calculates and returns the volume of an irregular tetrahedron.
+
+    Arguments:
+      nodes == [A, B, C, D]   list of corners of the tetrahedron with
+                              P == (p_x, p_y, p_2) being the Cartesian
+                              coordinates of point P for P in A, B, C, D
+
+    Returns:
+      volume V (float)
+    """
+
+    (A, B, C, D) = nodes
+
+    # vectors defining the tetrahedron
+    AB = B - A
+    AC = C - A
+    AD = D - A
+    
+    # normal vector on the ABC plane
+    n = np.cross(AB,AC)
+    n_len = np.linalg.norm(n)
+    n /= n_len
+
+    # area of the triangle ABC
+    A_ABC = 0.5*n_len
+
+    # distance of D from the ABC plane
+    dist_D = abs(np.dot(AD,n))
+
+    # volume
+    V = 1./3.*A_ABC*dist_D
+
+    return V
+
+def tetrahedron_volume_heron(nodes):
+    """
+    Calculates and returns the volume of an irregular tetrahedron.
+    The volume is calculated according to the generalized Heron formula.
+
+    Arguments:
+      nodes == [A, B, C, D]   list of corners of the tetrahedron with
+                              P == (p_x, p_y, p_2) being the Cartesian
+                              coordinates of point P for P in A, B, C, D
+
+    Returns:
+      volume V (float)
+    """
+
+    (A, B, C, D) = nodes
+
+    # edges of the tetrahedron following the naming convention
+    # at http://en.wikipedia.org/wiki/Heron%27s_formula (2013-04-05)
+    U = np.linalg.norm(B-A)
+    V = np.linalg.norm(C-A)
+    W = np.linalg.norm(C-B)
+    u = np.linalg.norm(D-C)
+    v = np.linalg.norm(D-B)
+    w = np.linalg.norm(D-A)
+
+    # auxiliary variables
+    X = (w - U + v)*(U + v + w)
+    x = (U - v + w)*(v - w + U)
+    Y = (u - V + w)*(V + w + u)
+    y = (V - w + u)*(w - u + V)
+    Z = (v - W + u)*(W + u + v)
+    z = (W - u + v)*(u - v + W)
+    a = np.sqrt(x*Y*Z)
+    b = np.sqrt(X*y*Z)
+    c = np.sqrt(X*Y*z)
+    d = np.sqrt(x*y*z)
+    
+    V  = (-a+b+c+d)*(a-b+c+d)*(a+b-c+d)*(a+b+c-d)
+    V  = np.sqrt(V)
+    V /= (192.0*u*v*w)
+
+    return V
+
+
+#---------------------- heights in a tetrahedron ----------------------#
+
+def tetrahedron_heights(nodes):
+    """
+    Calculates and returns the three four heights of an irregular tetrahedron.
+
+    Arguments:
+      nodes == [A, B, C, D]   list of corners of the tetrahedron with
+                              P == (p_x, p_y, p_2) being the Cartesian
+                              coordinates of point P for P in A, B, C, D
+
+    Returns:
+      volume list of heights
+    """
+
+    (A, B, C, D) = nodes
+
+    AB = B - A
+    AC = C - A
+    AD = D - A
+    BC = C - B
+    BD = D - B
+    
+    # normal vectors on the tetrahedron faces
+    n = np.cross(AB,AC)
+    n_ABC = n/np.linalg.norm(n)
+    n = np.cross(AC,AD)
+    n_ACD = n/np.linalg.norm(n)
+    n = np.cross(AB,AD)
+    n_ABD = n/np.linalg.norm(n)
+    n = np.cross(BC,BD)
+    n_BCD = n/np.linalg.norm(n)
+
+    # heights
+    h_A = abs(np.dot(AB,n_BCD))
+    h_B = abs(np.dot(AB,n_ACD))
+    h_C = abs(np.dot(AC,n_ABD))
+    h_D = abs(np.dot(AD,n_ABC))
+
+    return np.sort([h_A, h_B, h_C, h_D])
+
+#------------------- circumsphere of a tetrahedron --------------------#
+
+def circumsphere(nodes):
+
+    (A, B, C, D) = nodes
+    A = np.array(A)
+    B = np.array(B)
+    C = np.array(C)
+    D = np.array(D)
+
+    f  = A[2]*(B[1]*(D[0]-C[0]) - C[1]*(D[0]-B[0]) + D[1]*(C[0]-B[0])) 
+    f -= B[2]*(A[1]*(D[0]-C[0]) - C[1]*(D[0]-A[0]) + D[1]*(C[0]-A[0])) 
+    f += C[2]*(A[1]*(D[0]-B[0]) - B[1]*(D[0]-A[0]) + D[1]*(B[0]-A[0])) 
+    f -= D[2]*(A[1]*(C[0]-B[0]) - B[1]*(C[0]-A[0]) + C[1]*(B[0]-A[0]))
+
+    if (f <= EPS):
+        sys.stderr.write(
+            "Error: There is no circumsphere. "
+            + "All point lie in the same plain.")
+        sys.exit()
+
+    f = 0.5/f
+
+    xyz1 = A*A
+    xyz2 = B*B
+    xyz3 = C*C
+    xyz4 = D*D
+
+    d21  = xyz2 - xyz1
+    d31  = xyz3 - xyz1
+    d41  = xyz4 - xyz1
+    d32  = xyz3 - xyz2
+    d42  = xyz4 - xyz2
+    d43  = xyz4 - xyz3
+
+    x0   =  f*( A[2]*(B[1]*d43 - C[1]*d42 + D[1]*d32) 
+              - B[2]*(A[1]*d43 - C[1]*d41 + D[1]*d31) 
+              + C[2]*(A[1]*d42 - B[1]*d41 + D[1]*d21) 
+              - D[2]*(A[1]*d32 - B[1]*d31 + C[1]*d21) )
+
+    y0   = -f*( A[2]*(B[0]*d43 - C[0]*d42 + D[0]*d32) 
+              - B[2]*(A[0]*d43 - C[0]*d41 + D[0]*d31) 
+              + C[2]*(A[0]*d42 - B[0]*d41 + D[0]*d21) 
+              - D[2]*(A[0]*d32 - B[0]*d31 + C[0]*d21) )
+
+    z0   =  f*( A[1]*(B[0]*d43 - C[0]*d42 + D[0]*d32) 
+              - B[1]*(A[0]*d43 - C[0]*d41 + D[0]*d31) 
+              + C[1]*(A[0]*d42 - B[0]*d41 + D[0]*d21) 
+              - D[1]*(A[0]*d32 - B[0]*d31 + C[0]*d21) )
+
+    O = np.array([x0,y0,z0])
+    R = np.linalg.norm(O - A)
+ 
+    return (O, R)
+
+
+#----------------------------------------------------------------------#
+
+def analyze_sites(infile, r_cut, site_species='Li', frame_species='O',
+                  tet_only=False, verbose=False, NNN=False, dd=1.0):
+
+    struc  = Poscar.from_file(infile).structure
+    avec   = np.array(struc.lattice.matrix)
+    coords = np.array(struc.frac_coords)
+    cart   = np.dot(coords, avec)
+    types  = np.array([s.symbol for s in struc.species])
+    
+    nblist = NeighborList(coords, lattice_vectors=avec, types=types, 
+                          interaction_range=r_cut)
+
+    sites = np.arange(len(coords))[types == site_species]
+
+    dist_min  = []
+    dist_av   = []
+    volumes   = []
+
+    for s in sites:
+
+        (nbl, d, T) = nblist.get_neighbors_and_distances(s)
+        idx = (types[nbl] == frame_species)
+        nbl = np.array(nbl)[idx]
+        T   = np.array(T)[idx]
+        d   = np.array(d)[idx]
+
+        if NNN:
+            # filter out first neighbor shell:
+            d_min = np.min(d)
+            idx = (d > d_min + dd)
+            nbl = nbl[idx]
+            T   = T[idx]
+            d   = d[idx]
+            idx = np.argsort(d)
+            nbl = nbl[idx]
+            T   = T[idx]
+            d   = d[idx]
+            if (len(nbl)>8):
+                nbl = nbl[0:8]
+                T   = T[0:8]
+                d   = d[0:8]
+
+        if (tet_only and len(nbl) != 4):
+            continue
+
+        d_min = np.min(d)
+        d_av  = np.sum(d)/len(d)
+        
+        # nodes defining the site polyhedron
+        nodes = cart[nbl] + np.dot(T, avec)
+
+        # compute volume as sum of the volumina of the 
+        # Delaunay tetrahedrons 
+        vertices = Delaunay(nodes).vertices
+        V  = 0.0
+        for tet in vertices:
+            V += tetrahedron_volume(nodes[tet])
+
+        if tet_only:
+            H = tetrahedron_heights(nodes)
+            print("{:5d}  {:2s} {:2s} {:2d} {:8.4f} {:8.4f} {:6.2f} {:8.4f}".format(
+                s+1, types[s], frame_species, len(nodes), d_min, d_av, V, H[0]))            
+        else:
+            print("{:5d}  {:2s} {:2s} {:2d} {:8.4f} {:8.4f} {:6.2f}".format(
+                s+1, types[s], frame_species, len(nodes), d_min, d_av, V))
+
+        dist_min.append(d_min)
+        dist_av.append(d_av)
+        volumes.append(V)
+
+    if (len(volumes) > 0) and verbose:
+
+        V_av  = np.sum(volumes)/len(volumes)
+        V_max = np.max(volumes)
+        V_min = np.min(volumes)
+    
+        d_av  = np.sum(dist_av)/len(dist_av)
+        d_min = np.min(dist_min)
+
+        print("")
+        print(" average site volume: {:.2f} A^3".format(V_av))
+        print(" maximum site volume: {:.2f} A^3".format(V_max))
+        print(" minimum site volume: {:.2f} A^3".format(V_min))
+        print("")
+        print(" average average {}-{} distance: {:.3f} A".format(
+            site_species, frame_species, d_av))
+        print(" minimal minimal {}-{} distance: {:.3f} A".format(
+            site_species, frame_species, d_min))
+        print("")
+
+    sys.stdout.flush()
+
+#----------------------------------------------------------------------#
+
+if (__name__ == "__main__"):
+
+    parser = argparse.ArgumentParser(
+        description     = __doc__+"\n{} {}".format(__date__,__author__),
+        formatter_class = argparse.RawDescriptionHelpFormatter )
+
+    parser.add_argument(
+        "input_file",
+        help    = "Input file in VASP's POSCAR format.")
+
+    parser.add_argument(
+        "--range", "-r",
+        help    = "Interaction range (cut-off redius).",
+        type    = float,
+        default = "3.0",
+        dest    = "r_cut")
+
+    parser.add_argument(
+        "--site-species",
+        help    = "Species whose sites shall be analyzed (default: Li).",
+        type    = str,
+        default = "Li")
+
+    parser.add_argument(
+        "--frame-species",
+        help    = "Species that geometrically defines sites (default: O).",
+        type    = str,
+        default = "O")
+
+    parser.add_argument(
+        "--tetrahedrons", "-t",
+        help    = "Only analyse tetrahedral sites.",
+        action  = "store_true")
+
+    parser.add_argument(
+        "--verbose", "-v",
+        help    = "Print summary of the results.",
+        action  = "store_true")
+
+    parser.add_argument(
+        "--second-nearest", "--nnn",
+        help    = "Consider second nearest frame atoms.",
+        action  = "store_true",
+        dest    = "second_nearest")
+
+    args = parser.parse_args()
+
+    analyze_sites( infile        = args.input_file,
+                   r_cut         = args.r_cut,
+                   site_species  = args.site_species,
+                   frame_species = args.frame_species,
+                   tet_only      = args.tetrahedrons,
+                   verbose       = args.verbose,
+                   NNN           = args.second_nearest )
+
---
+++ old/mcpercol.py
@@ -0,0 +1,299 @@
+#!/usr/bin/env python
+
+"""
+Insert description here.
+"""
+
+from __future__ import print_function
+
+__author__ = "Alexander Urban"
+__date__   = "2013-02-20"
+
+import argparse
+import numpy as np
+
+from pymatgen.io.vaspio import Poscar
+from pypercol           import Lattice
+from pypercol           import IsingModel
+from pypercol           import Percolator
+from pypercol.ising     import k_B
+from pypercol.aux       import ProgressBar
+from pypercol.aux       import uprint
+
+EPS   = 100.0*np.finfo(np.float).eps
+
+#----------------------------------------------------------------------#
+
+def runmc(infile, T=300.0, J1=0.5e-3, J2=0.5e-3, H=0.0, Nequi=100,
+          NMC=500, Nstruc=250, supercell=(1,1,1), common=None,
+          conc=None, nocc=None, opt=False, lgh=False, mVT=False):
+
+    outfile = "mcpercol.out"
+
+    uprint("\n Reading structure from file '{}'...".format(infile), end="")
+    structure = Poscar.from_file(infile).structure
+    uprint(" done.")
+
+    uprint("\n Setting up lattice and neighbor lists...", end="")
+    lattice = Lattice.from_structure(structure, supercell=supercell)
+    lattice.get_nnn_shells()
+    uprint(" done.")
+    if conc:
+        uprint(" Random site occupations. p = {}".format(conc))
+        lattice.random_decoration(p=conc)
+    elif nocc:
+        uprint(" Random site occupations. N = {}".format(nocc))
+        lattice.random_decoration(N=nocc)
+    else:
+        uprint(" Initial site occupations taken from structure file.")
+    uprint(lattice)
+
+    if not opt:
+        uprint(" Initializing percolator...", end="")
+        percol = Percolator(lattice)
+        uprint(" done.")
+        if common > 0:
+            uprint(" Using percolation rule with {} common neighbor(s).".format(common))
+            percol.set_special_percolation_rule(num_common=common)
+
+    uprint("\n Initializing Ising model...", end="")
+    if lgh:
+        ising = IsingModel.from_lattice_gas_H(lattice, J1, J2, mu=H)
+    else:
+        ising = IsingModel(lattice, J1, J2, H=H)
+    uprint(" done.")
+    uprint(ising)
+
+    tau    = 1.0
+    kT_inv = 1.0/(k_B*T)
+
+    Nevery = int(round(float(NMC)/float(Nstruc)))
+
+    if opt:
+        Nequi = NMC
+        NMC = 0
+        Tfinal = 1.0
+        Tramp = (Tfinal - T)/(Nequi-1)
+        Nconst_max = 30
+        Nconst = 0
+        E_prev = 0.0
+        MC_eps = EPS*float(Nequi)
+
+    with open(outfile, 'w', 0) as f:
+
+        if opt:
+            uprint(" annealing for {} MC steps from T = {} to {}".format(Nequi,T,Tfinal))
+            E_low = ising.total_energy()
+        else:
+            uprint(" equilibrating for {} MC steps at T = {}".format(Nequi,T))
+
+        if mVT:
+            uprint(" simulating a micro-canonical ensemble (muVT)\n")
+        else:
+            uprint(" simulating a canonical ensemble (NVT)\n")
+
+        f.write("# step energy temperature concentration\n")
+
+        conv = False
+        pb = ProgressBar(Nequi)
+        for i in xrange(Nequi):
+            pb()
+            if mVT:
+                E = ising.mc_mVT(kT_inv=kT_inv, tau=tau)
+            else:
+                E = ising.mc_NVT(kT_inv=kT_inv, tau=tau)
+            p = float(lattice.num_occupied)/float(lattice.num_sites)
+            f.write("{} {} {} {}\n".format(i, E, T, p))
+            if opt and (i < Nequi-1):
+                T += Tramp
+                T = max(T,1.0)
+                kT_inv = 1.0/(k_B*T)
+                E_low = min(E, E_low)
+                if (abs(E - E_prev) <= MC_eps):
+                    Nconst += 1
+                    if (Nconst >= Nconst_max) and (E == E_low):
+                        uprint(" done.")
+                        uprint(" Converged after {} MC steps (T = {}).\n".format(i, T))
+                        conv = True
+                        break
+                else:
+                    E_prev = E
+                    Nconst = 0
+        if not conv:
+            pb()
+            if opt and (abs(E - E_low) > MC_eps):
+                uprint(" Warning: final state is {} higher in energy ".format(E-E_low)
+                       + "then lowest encountered !")
+
+        p_percol = 0.0
+        f_percol = 0.0
+        if (NMC > 0):
+            uprint(" now sampling {} structures ".format(Nstruc)
+                   + "out of {} MC steps".format(NMC))
+
+            nsamp = 0    # number of sampled structures
+            nwrap = 0.0  # sum of fractions of percolating sites
+            nperc = 0    # number of percolating structures
+            pb = ProgressBar(NMC)
+            for i in xrange(Nequi,NMC+Nequi):
+                pb()
+                if mVT:
+                    E = ising.mc_mVT(kT_inv=kT_inv, tau=tau)
+                else:
+                    E = ising.mc_NVT(kT_inv=kT_inv, tau=tau)
+                p = float(lattice.num_occupied)/float(lattice.num_sites)
+                f.write("{} {} {} {}\n".format(i, E, T, p))
+                if (i % Nevery == 0):
+                    nsamp += 1
+                    wrapping_fraction = percol.check_spanning()
+                    if (wrapping_fraction > 0.0):
+                        nwrap += wrapping_fraction
+                        nperc += 1
+            pb()
+
+            p_percol = float(nperc)/float(nsamp)
+            f_percol = float(nwrap)/float(nsamp)
+
+            uprint(" percolation probability (fraction): {} ({})".format(
+                p_percol, f_percol))
+
+        E_tot = ising.total_energy()
+        if (abs(E-E_tot)>1.0e-6):
+            uprint("Error: final energy inconsistent - check MC code!")
+
+        uprint("  E_tot         T             J1            "
+               + "J2            H             J1/kT         p"
+               + "              p_percol      f_percol")
+        uprint(" {:.6e}  {:.6e}  {:.6e}  {:.6e}  ".format(E_tot, T, J1, J2)
+               + "{:.6e}  {:.6e}  {:.6f}  {:.6f}  {:.6f}\n".format(
+                 H, J1*kT_inv, float(lattice.num_occupied)/float(lattice.num_sites),
+                 p_percol, f_percol))
+
+    uprint(" Saving final structure to file 'CONTCAR'.")
+    lattice.save_structure('CONTCAR')
+
+    uprint("")
+
+#----------------------------------------------------------------------#
+
+if (__name__ == "__main__"):
+
+    parser = argparse.ArgumentParser(
+        description     = __doc__+"\n{} {}".format(__date__,__author__),
+        formatter_class = argparse.RawDescriptionHelpFormatter )
+
+    parser.add_argument(
+        "structure",
+        help    = "structure in VASP's extended POSCAR format",
+        default = "POSCAR",
+        nargs   = "?")
+
+    parser.add_argument(
+        "--common",
+        help    = "Number of common neighbors for two sites to be percolating.",
+        type    = int,
+        default = 0)
+
+    parser.add_argument(
+        "-c", "--concentration",
+        help    = "Concentration of occupied sites.",
+        type    = float,
+        default = None,
+        dest    = "conc")
+
+    parser.add_argument(
+        "-n", "--num-occupied",
+        help    = "Exact number of occupied sites.",
+        type    = int,
+        default = None,
+        dest    = "nocc")
+
+    parser.add_argument(
+        "--supercell",
+        help    = "List of multiples of the lattice cell" +
+                  " in the three spacial directions",
+        type    = int,
+        default = (1,1,1),
+        nargs   = "+")
+
+    parser.add_argument(
+        "-T", "--temperature",
+        help    = "MC temperature.",
+        type    = float,
+        default = 300.0,
+        dest    = "T")
+
+    parser.add_argument(
+        "--V1", "--J1",
+        help    = "Nearest neighbor interaction.",
+        type    = float,
+        dest    = "J1",
+        default = 0.3e-2)
+
+    parser.add_argument(
+        "--V2", "--J2",
+        help    = "Next nearest neighbor interaction.",
+        type    = float,
+        dest    = "J2",
+        default = 0.3e-2)
+
+    parser.add_argument(
+        "-H", "--mu",
+        help    = "Magnetic field term (point interaction).",
+        type    = float,
+        default = 0.0,
+        dest    = "H")
+
+    parser.add_argument(
+        "--opt",
+        help    = "Only optimize structure (search ground state).",
+        action  = "store_true")
+
+    parser.add_argument(
+        "--LGH",
+        help    = "Initialize as Lattice gas Hamiltonian (v1, v2, mu).",
+        action  = "store_true")
+
+    parser.add_argument(
+        "--mVT",
+        help    = "Micro-canonical ensemble (variable particle number).",
+        action  = "store_true")
+
+    parser.add_argument(
+        "--N-equi",
+        help    = "Number of MC steps for equilibration.",
+        type    = int,
+        default = 100,
+        dest    = "N_equi")
+
+    parser.add_argument(
+        "--N-MC",
+        help    = "Number of MC steps for the sampling (after equilibration).",
+        type    = int,
+        default = 250,
+        dest    = "N_MC")
+
+    parser.add_argument(
+        "--N-samp",
+        help    = "Number of structures to sample for percolation properties.",
+        type    = int,
+        default = 250,
+        dest    = "N_samp")
+
+    args = parser.parse_args()
+
+    runmc( infile    = args.structure,
+           T         = args.T,
+           J1        = args.J1,
+           J2        = args.J2,
+           H         = args.H,
+           Nequi     = args.N_equi,
+           NMC       = args.N_MC,
+           Nstruc    = args.N_samp,
+           supercell = args.supercell,
+           common    = args.common,
+           conc      = args.conc,
+           nocc      = args.nocc,
+           opt       = args.opt,
+           lgh       = args.LGH,
+           mVT       = args.mVT)
